d3177b4f855abafea00a9c87e8f68f0f88fb61e1
Reossann <reossan.1003@icloud.com>
2025-05-19 00:00:51 +0900

    盤面の優劣表示完成（cssはまだまだ）

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,7 +61,11 @@ const vision = (board: number[][], turnColor: number) => {
         for (const [dy, dx] of directions) {
           if (board[y + dy] !== undefined && board[y + dy][x + dx] === 2 / turnColor) {
             for (let num1 = 1; num1 < 9; num1++) {
-              if (board[y + dy * num1] === undefined || board[y + dy * num1][x + dx * num1] === 0) {
+              if (
+                board[y + dy * num1] === undefined ||
+                board[y + dy * num1][x + dx * num1] === 0 ||
+                board[y + dy * num1][x + dx * num1] === 3
+              ) {
                 break;
               }
               if (board[y + dy * num1][x + dx * num1] === turnColor) {
@@ -74,6 +78,31 @@ const vision = (board: number[][], turnColor: number) => {
     }
   }
   return newBoard;
+};
+
+const result = (a: number, b: number, c: number) => {
+  if (a === b) {
+    const tx = '均衡中。。。';
+    return tx;
+  }
+  if (c === undefined) {
+    if (a > b) {
+      const tx = '黒勝利！！';
+      return tx;
+    }
+    if (a < b) {
+      const tx = '白勝利！！';
+      return tx;
+    }
+  }
+  if (a < b) {
+    const tx = '白優勢！！！！';
+    return tx;
+  }
+  if (a > b) {
+    const tx = '黒優勢！！！！';
+    return tx;
+  }
 };
 
 export default function Home() {
@@ -123,10 +152,7 @@ export default function Home() {
   console.log(counts[3]);
   const count1 = counts[1];
   const count2 = counts[2];
-  const tx = '';
-  if (counts[0] === undefined && counts[3] === undefined) {
-    console.log(100);
-  }
+  const txs = result(counts[1], counts[2], counts[0]);
 
   return (
     <div className={styles.container}>
@@ -146,7 +172,7 @@ export default function Home() {
         </div>
       </div>
       <div className={styles.scoreBoard4}>
-        <p style={{ color: 'purple', fontSize: 100 }}>{tx}</p>
+        <p style={{ color: 'purple', fontSize: 100 }}>{txs}</p>
       </div>
       <div className={styles.board}>
         {p.map((row, y) =>
